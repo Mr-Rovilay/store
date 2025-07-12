8a3eff99755f228b39ed7eb211226ae63676c084
ayodeji akinola <ayodejiakinola007@gmail.com>
2025-07-12 22:28:58 +0100

    mkl

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import HeaderMenu from './HeaderMenu'
+import Logo from './Logo'
+import Container from './Container'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="border-b border-gray-200 bg-white shadow-sm py-5">
+    <Container className='flex justify-between items-center gap-7 text-lightColor dark:text-darkColor'>
+   
+        <HeaderMenu />
+        <Logo />
+    
+      <div className="">
+        {/* Add any additional header elements here, like search or cart */}
+        right
+      </div>
+    </Container>
+    </header>
   )
 }
 
