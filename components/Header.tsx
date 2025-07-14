15f8da0d83f352eba3372542084bc4f05fa5c270
ayodeji akinola <ayodejiakinola007@gmail.com>
2025-07-14 21:43:17 +0100

    sa

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,10 +6,13 @@ import MobileMenu from "./MobileMenu";
 import CartIcon from "./CartIcon";
 import SearchBar from "./SearchBar";
 import { currentUser } from "@clerk/nextjs/server";
-import { SignInButton } from "@clerk/nextjs";
+import { ClerkLoaded, SignedIn, SignInButton, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import { ListOrdered } from "lucide-react";
 
 const Header = async () => {
   const user =  await currentUser()
+  console.log(user)
   return (
     <header className="border-b border-gray-200 bg-white shadow-sm py-5">
       <Container className="flex justify-between items-center gap-7 text-lightColor dark:text-darkColor">
@@ -22,11 +25,22 @@ const Header = async () => {
           {/* Add any additional header elements here, like search or cart */}
           <SearchBar/>
          <CartIcon/>
+         <ClerkLoaded>
+          <SignedIn>
+<Link href={"/orders"} className='group relative'>
+        <ListOrdered className='w-5 h-5 group-hover:text-darkColor hoverEffect '/>
+        <span className="absolute -top-1 -right-1 bg-darkColor text-white h-3.5 w-3.5 rounded-full text-xs font-semibold flex item-center justify-center">0</span>
+    </Link>
+    <UserButton/>
+          </SignedIn>
+
          {
           !user && ( <SignInButton mode="modal">
             <button className="text-sm font-semibold hover:text-darkColor hoverEffect">Sign In</button>
           </SignInButton>)
          }
+
+         </ClerkLoaded>
         </div>
       </Container>
     </header>
